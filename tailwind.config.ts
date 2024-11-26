d14fdd231aa8c9948947388e7f2dc367928b19a6
AnthonyDWeb <delforge.anthony@gmail.com>
2024-11-26 17:53:15 +0100

    try to fix deploy issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ const config = {
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
-		"./context/**/*.{ts,tsx}",
 		"./providers/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
