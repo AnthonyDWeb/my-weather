fecb0fa921d3c4483dd46f83a38739f87797a3e8
AnthonyDWeb <delforge.anthony@gmail.com>
2024-11-26 18:36:51 +0100

    test fix issue 5

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -2,21 +2,22 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  try {
-    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+	try {
+		const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
-    const searchParams = req.nextUrl.searchParams;
+		const searchParams = req.nextUrl.searchParams;
 
-    const lat = searchParams.get("lat");
-    const lon = searchParams.get("lon");
+		const lat = searchParams.get("lat");
+		const lon = searchParams.get("lon");
+		if (lat && lon) {
+			const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+			const res = await axios.get(url);
 
-    const res = await axios.get(url);
-
-    return NextResponse.json(res.data);
-  } catch (error) {
-    console.log("Error fetching forecast data");
-    return new Response("Error fetching forecast data", { status: 500 });
-  }
+			return NextResponse.json(res.data);
+		}
+	} catch (error) {
+		console.log("Error fetching forecast data");
+		return new Response("Error fetching forecast data", { status: 500 });
+	}
 }
